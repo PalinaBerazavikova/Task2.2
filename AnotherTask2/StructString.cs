803a048d45255ae9d09b332ef9daa9debec7b0cb
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-09-21 18:06:36 +0300

    notAll

---
+++ AnotherTask2/StructString.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherTask2
+{
+    class StructString
+    {
+        List<string> newListString = new List<string>();
+        List<int> resultIntList = new List<int>();
+        List<double> resultDoubleList = new List<double>();
+        public int NumberOfInt { get; set; }
+        private int numberOfInt;
+        public int SumInt{ get; set; }
+        private int sumInt;
+        public double SumDouble { get; set; }
+        private double sumDouble;
+        public int NumberOfDouble { get; set; }
+        private int numberOfDouble;
+        private const char space = ' ';
+
+        public int FindNumbers(List<string> list)
+        {
+            int resultInt;
+            this.NumberOfInt = 0;
+            foreach (string str in list)
+            {
+                if (Int32.TryParse(str, out resultInt))
+                {
+                    this.resultIntList.Add(resultInt);
+                    this.NumberOfInt++;
+                }
+            }
+            return NumberOfInt;
+        }
+
+        public int FindDouble(List<string> list)
+        {
+            double resultDouble;
+            this.NumberOfDouble = 0;
+            foreach (string str in list)
+            {
+                if (Double.TryParse(str, out resultDouble))
+                {
+                    if (resultDouble % 1 > 0)
+                    {
+                        resultDoubleList.Add(resultDouble);
+                        this.NumberOfDouble++;
+                    }
+                }
+            }
+            return this.NumberOfDouble;
+        }
+
+        public void FindStrings(List<string> list)
+        {
+            foreach (string str in list)
+            {
+                double resultDouble;
+                int resultInt;
+                if ((!Double.TryParse(str, out resultDouble)) && (!Int32.TryParse(str, out resultInt)))
+                {
+                    this.newListString.Add(str);
+                }
+            }
+            this.PrintStrings(newListString);
+        }
+
+        public void PrintAll()
+        {
+            this.PrintInt(resultIntList);
+            this.PrintDouble(resultDoubleList);
+            
+            this.PrintStrings(this.newListString);
+        }
+
+        public void PrintInt(List<int> list)
+        {
+            this.SumInt = 0;
+            foreach (int output in list)
+            {
+                Console.WriteLine($"Number: {output}".PadLeft(15, space));
+                this.SumInt = this.SumInt + output;
+            }
+            Console.WriteLine($"Average: {(double)this.SumInt / (double)list.Count}".PadLeft(15, space));
+        }
+
+        public void PrintDouble(List<double> list)
+        {
+            foreach (double outputDouble in list)
+            {
+                Console.WriteLine($"Double: {outputDouble:#.##}".PadLeft(15, space));
+                this.SumDouble = this.SumDouble + outputDouble;
+            }
+            Console.WriteLine($"Average: {this.SumDouble / resultDoubleList.Count:#.##}".PadLeft(15, space));
+        }
+
+        public void PrintStrings(List<string> list)
+        {
+            list.Sort();
+            foreach (string lines in list)
+            {
+                Console.WriteLine(lines);
+            }
+        }
+    }
+}
---
+++ AnotherTask2/Talker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherTask2
+{
+    class Talker
+    {
+        private string input;
+        public string Input { get; set; }
+        private const string Stop = "stop";
+        private const string Enter = "Enter String. To quit enter stop";
+        private const string NumbOfInt = "Number of Int";
+        private const string NumbOfDouble = "Number of Double";
+        private List<string> listString = new List<string>();
+        StructString ss = new StructString();
+
+        public void EnterStrings()
+        {
+            this.Input = string.Empty;
+            Console.WriteLine(Enter);
+            while (!this.Input.Equals(Stop))
+            {
+                if (!string.IsNullOrEmpty(this.Input))
+                {
+                    this.listString.Add(this.Input);
+                }
+                this.Input = Console.ReadLine();
+            }
+            Console.WriteLine($"{NumbOfInt} {ss.FindNumbers(this.listString)}");
+            Console.WriteLine($"{NumbOfDouble} {ss.FindDouble(this.listString)}");
+            ss.PrintAll();
+            ss.FindStrings(listString);
+            
+        }
+
+
+
+
+    }
+}
